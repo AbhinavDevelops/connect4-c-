172e2eba4e90e2d06766ef8a07b5b21c7417e7d6
AbhinavDevelops <abhinav.rajaram02@gmail.com>
2024-08-05 18:14:57 +0800

    core functionality works and parses misinformed user input

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-#define print(x) cout << x << endl
+#define print(x) cout << (x) << endl
 
 void printBoard(string board[6][7])
 {
@@ -25,13 +25,20 @@ void printBoard(string board[6][7])
 bool checkDirection(string board[6][7], int r, int c, int rowAdd, int columnAdd, string userCoin)
 {
     int count = 0;
-    while (r < 6 && c < 7)
+
+    while (r >= 0 && r < 6 && c >= 0 && c < 7)
     {
+
+        // print("row:" << r << "column:" << c);
 
         // print(board[r][c]);
 
         if (board[r][c] == userCoin)
+        {
+
+            // print("diagonal match found row:" << r << "column:" << c);
             count += 1;
+        }
 
         else
             return (count >= 4);
@@ -56,18 +63,31 @@ bool checkWinners(string board[6][7], string userCoin)
         for (int j = 0; j < 7; j++)
         {
 
-            // horizontal
+            if (board[i][j] == userCoin)
+            {
 
-            if (checkDirection(board, i, j, 0, 1, userCoin))
-                return true;
+                // horizontal
 
-            // verctical
+                if (checkDirection(board, i, j, 0, 1, userCoin))
+                    return true;
 
-            if (checkDirection(board, i, j, 1, 0, userCoin))
-                return true;
+                // verctical
 
-            if (checkDirection(board, i, j, -1, -1, userCoin))
-                return true;
+                if (checkDirection(board, i, j, 1, 0, userCoin))
+                    return true;
+
+                if (checkDirection(board, i, j, -1, -1, userCoin))
+                    return true;
+
+                if (checkDirection(board, i, j, -1, 1, userCoin))
+                    return true;
+
+                if (checkDirection(board, i, j, 1, -1, userCoin))
+                    return true;
+
+                if (checkDirection(board, i, j, 1, 1, userCoin))
+                    return true;
+            }
         }
     }
 
@@ -79,22 +99,48 @@ void playerTurn(int pickedRow, string board[6][7], string userCoin)
 
     int row = pickedRow - 1;
 
-    print(row);
-
-    if (board[-1][row] == "e")
+    if (board[5][row] == "*")
     {
-        board[-1][row] = userCoin;
+        board[5][row] = userCoin;
         return;
     }
 
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < 5; i++)
     {
-        if (board[i + 1][row] != "e")
+        if (board[i + 1][row] != "*")
         {
             board[i][row] = userCoin;
+            return;
         }
 
         // print(board[i][row]);
+    }
+}
+
+int getPick()
+{
+
+    int pickedRow;
+
+    while (true)
+    {
+
+        string uinput;
+
+        cin >> uinput;
+
+        if (isdigit(uinput[0]))
+        {
+
+            pickedRow = atoi(uinput.c_str());
+
+            if (0 < pickedRow && pickedRow < 8)
+                return pickedRow;
+            else
+                print("Please pick a number between 1 and 7 inclusive");
+        }
+        else
+            print("Please enter a valid number");
     }
 }
 
@@ -105,12 +151,13 @@ int main()
 
     string board[6][7] = {
 
-        {"e", "e", "e", "e", "e", "e", "e"},
-        {"e", "e", "e", "e", "e", "e", "e"},
-        {"e", "e", "e", "e", "e", "e", "e"},
-        {"e", "e", "e", "e", "e", "e", "e"},
-        {"e", "e", "e", "e", "e", "e", "e"},
-        {"e", "e", "e", "e", "e", "e", "e"},
+        {"*", "*", "*", "*", "*", "*", "*"},
+        {"*", "*", "*", "*", "*", "*", "*"},
+        {"*", "*", "*", "*", "*", "*", "*"},
+        {"*", "*", "*", "*", "*", "*", "*"},
+        {"*", "*", "*", "*", "*", "*", "*"},
+        {"*", "*", "*", "*", "*", "*", "*"}
+
     };
     printBoard(board);
 
@@ -119,14 +166,14 @@ int main()
 
     print("Player 1 you are X, Player 2 you are O\n\n\n");
 
-    while (play)
+    while (true)
     {
 
         print("Player 1 Pick a row between 1-7 to drop your coin\n");
 
         int pickedRow1;
 
-        cin >> pickedRow1;
+        pickedRow1 = getPick();
 
         playerTurn(pickedRow1, board, userCoin1);
 
@@ -138,11 +185,12 @@ int main()
 
             return 0;
         }
+
         print("Player 2 Pick a row between 1-7 to drop your coin\n");
 
         int pickedRow2;
 
-        cin >> pickedRow2;
+        pickedRow2 = getPick();
 
         playerTurn(pickedRow2, board, userCoin2);
 
@@ -155,16 +203,16 @@ int main()
             return 0;
         }
 
-        print("Do you want to keep playing. y:yes n:no");
+        // print("Do you want to keep playing. y:yes n:no");
 
-        string input;
+        // string input;
 
-        cin >> input;
+        // cin >> input;
 
-        if (input == "y" or input == "yes")
-            play = 1;
+        // if (input == "y" or input == "yes")
+        //     play = 1;
 
-        else
-            play = 0;
+        // else
+        //     play = 0;
     }
 }
